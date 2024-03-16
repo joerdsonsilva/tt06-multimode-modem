9580e1e9c0f116d0e6c0ecf7cc2677c51e400506
joerdsonsilva <75455785+joerdsonsilva@users.noreply.github.com>
2024-03-16 15:52:04 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,32 +1,40 @@
-# SPDX-FileCopyrightText: © 2023 Uri Shaked <uri@tinytapeout.com>
-# SPDX-License-Identifier: MIT
-
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
+
 
 @cocotb.test()
-async def test_adder(dut):
-  dut._log.info("Start")
-  
-  # Our example module doesn't use clock and reset, but we show how to use them here anyway.
-  clock = Clock(dut.clk, 10, units="us")
-  cocotb.start_soon(clock.start())
+async def test_modem(dut):
+    dut._log.info("start")
+    clock = Clock(dut.clk, 2000, units="us")
+    cocotb.start_soon(clock.start())
 
-  # Reset
-  dut._log.info("Reset")
-  dut.ena.value = 1
-  dut.ui_in.value = 0
-  dut.uio_in.value = 0
-  dut.rst_n.value = 0
-  await ClockCycles(dut.clk, 10)
-  dut.rst_n.value = 1
+    # reset
+    dut._log.info("reset")
+    dut.rst_n.value = 0
+    await Timer(200, units="us")
 
-  # Set the input values, wait one clock cycle, and check the output
-  dut._log.info("Test")
-  dut.ui_in.value = 20
-  dut.uio_in.value = 30
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 1200)
 
-  await ClockCycles(dut.clk, 1)
+    dut.ui_in <= 0b00
+    await ClockCycles(dut.clk, 500)
 
-  assert dut.uo_out.value == 50
+    dut.ui_in <= 0b01
+    await ClockCycles(dut.clk, 2100)
+
+    dut.ui_in <= 0b10
+    await ClockCycles(dut.clk, 2100)
+
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 500)
+
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 300)
+
+    dut.ui_in <= 0b11
+    await ClockCycles(dut.clk, 2000)
+
+    dut.ui_in <= 0b00
+    await ClockCycles(dut.clk, 500)
+
